5ac2ac97362304ef0d52aa0e3c943e9abeb36bed
luby404 <ricardokayoca@gmail.com>
2026-03-11 22:14:01 +0100

    auth settings

--- ext/autn.py
+++ ext/autn.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask_login import LoginManager
+from models import Usuario
 
 
+login = LoginManager()
+login.login_view = "auth.login"
+
+@login.user_loader
+def load_user_auth(id):
+    return Usuario.get_or_none(Usuario.id == id)
 
 def init_auth(app: Flask):
-    ...
+    login.init_app(app)
